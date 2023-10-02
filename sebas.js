3e8fd225010fd828a0b0d079779b77df20e5523f
SV1992 <116411942+SV1992@users.noreply.github.com>
2023-10-02 17:26:54 -0500

    Add files via upload

--- sebas.js
+++ sebas.js
@@ -1,15 +1,19 @@
 function abrirPDF(carnetURL) {
     window.open(carnetURL, '_blank');
 }
+const nosotrostaButton = document.getElementById('nosotros');
+nosotrostaButton.addEventListener('click', function () {
+    abrirPDF('https://drive.google.com/file/d/1S17x8IqmzwOj8TJq6Qdp0juljjezkRw7/view?usp=drive_link');
+});
 
 const juanButton = document.getElementById('juan');
 juanButton.addEventListener('click', function () {
-    abrirPDF('https://drive.google.com/file/d/15iJZXoi-hWcuXdF64aEKc0ujl3U3o6nO/view?usp=drive_link');
+    abrirPDF('https://drive.google.com/file/d/1G10QPSNzts15QuCJU9yCtvC6jM68HQjK/view?usp=drive_link');
 });
 
 const sebasButton = document.getElementById('sebas');
 sebasButton.addEventListener('click', function () {
-    abrirPDF('https://drive.google.com/file/d/1vhQCx9CF6lX5QLYJmQr07MON_46Kx5qb/view?usp=drive_link');
+    abrirPDF('https://drive.google.com/file/d/12XKHT8wzFXv_xlT_D1B5WwEnKiDHwt3B/view?usp=drive_link');
 });
 
 const pendonButton = document.getElementById('pendon');
